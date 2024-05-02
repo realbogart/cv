79074102ad46d808993beba630e9545e36176892
Johan Yngman <johan.yngman@gmail.com>
2024-05-02 11:24:47 +0200

    Add --wait-util networkidle0 to puppeteer command

--- flake.nix
+++ flake.nix
@@ -43,7 +43,7 @@
             echo "Building CV..."
             mkdir $out
             sleep 1
-            puppeteer print --margin-top 0 --margin-right 0 --margin-bottom 0 --margin-left 0 index.html $out/cv.pdf
+            puppeteer print --wait-until networkidle0 --margin-top 0 --margin-right 0 --margin-bottom 0 --margin-left 0 index.html $out/cv.pdf
           '';
         };
         # apps.default = {
