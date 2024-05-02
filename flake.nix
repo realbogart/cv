b231a001ddec2a10cadae350dcde66c40e2d77c4
Johan Yngman <johan.yngman@gmail.com>
2024-05-02 11:31:14 +0200

    Removed sleep

--- flake.nix
+++ flake.nix
@@ -42,7 +42,6 @@
             echo "FONT_PATH set to $FONT_PATH"
             echo "Building CV..."
             mkdir $out
-            sleep 1
             puppeteer print --wait-until networkidle0 --margin-top 0 --margin-right 0 --margin-bottom 0 --margin-left 0 index.html $out/cv.pdf
           '';
         };
