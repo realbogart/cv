faad8e95e03d624c76aa67402d85a0d9725b250e
Johan Yngman <johan.yngman@gmail.com>
2024-05-03 23:16:30 +0200

    test

--- convert.py
+++ convert.py
@@ -15,9 +15,9 @@ file_name = os.path.join(current_script_dir, 'index.html')
 async def main():
     browser = await launch({ 'headless': True, 'executablePath': 'chromium', 'args': ['--no-sandbox', '--disable-web-security', '--disable-setuid-sandbox'] })
     page = await browser.newPage()
-    await page.goto(f'file://{file_name}', {'waitUntil': 'networkidle0'})
+    await page.goto(f'file://{file_name}', {'waitUntil': 'load'})
     await page.waitForFunction('document.fonts.ready');
-    await page.evaluateHandle('document.fonts.ready');
+    # await page.evaluateHandle('document.fonts.ready');
     # time.sleep(1)
     await page.pdf({ 'path': output_file_path, 'format': 'A4', 'printBackground': True, 'margin': { 'top': 0, 'bottom': 0, 'left': 0, 'right': 0 } });
     await browser.close()
