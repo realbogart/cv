d26ffc112d378e7015a289c0118a07eaa0af91bf
Johan Yngman <johan.yngman@gmail.com>
2024-05-03 20:01:08 +0200

    Add pyppetter script

---
+++ build-cv-pyppeteer.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python convert.py cv.pdf && xdg-open cv.pdf
--- convert.py
+++ convert.py
@@ -1,11 +1,21 @@
 import asyncio
+import os
+import sys
 from pyppeteer import launch
+
+if len(sys.argv) < 2:
+    print("Usage: python convert.py <output_file_path>")
+    sys.exit(1)
+
+output_file_path = sys.argv[1]
+current_script_dir = os.path.dirname(os.path.abspath(__file__))
+file_name = os.path.join(current_script_dir, 'index.html')
 
 async def main():
     browser = await launch({ 'headless': True, 'executablePath': 'chromium' })
     page = await browser.newPage()
-    await page.goto('file:///home/johan/projects/cv/index.html', {'waitUntil': 'networkidle0'})
-    await page.pdf({ 'path': 'pyppeteer.pdf', 'format': 'A4', 'printBackground': True, 'margin': { 'top': 0, 'bottom': 0, 'left': 0, 'right': 0 } });
+    await page.goto(f'file://{file_name}', {'waitUntil': 'networkidle0'})
+    await page.pdf({ 'path': output_file_path, 'format': 'A4', 'printBackground': True, 'margin': { 'top': 0, 'bottom': 0, 'left': 0, 'right': 0 } });
     await browser.close()
 
 asyncio.get_event_loop().run_until_complete(main())
