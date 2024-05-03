193a26cf7e65eab1029fc4ee24a74c86faf02e78
Johan Yngman <johan.yngman@gmail.com>
2024-05-03 18:23:59 +0200

    pdf formatting

--- convert.py
+++ convert.py
@@ -5,7 +5,7 @@ async def main():
     browser = await launch({ 'headless': True, 'executablePath': 'chromium' })
     page = await browser.newPage()
     await page.goto('file:///home/johan/projects/cv/index.html', {'waitUntil': 'networkidle0'})
-    await page.pdf({ 'path': 'pyppeteer.pdf', 'format': 'A4' });
+    await page.pdf({ 'path': 'pyppeteer.pdf', 'format': 'A4', 'printBackground': True, 'margin': { 'top': 0, 'bottom': 0, 'left': 0, 'right': 0 } });
     await browser.close()
 
 asyncio.get_event_loop().run_until_complete(main())
